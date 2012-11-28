d4c6b759de1ac73b82e1e19b8a2f799167564937
Akshay Joshi <me@akshayjoshi.com>
2012-11-27 20:25:27 -0800

    Add a logging stub.

--- upgrade_check_tool/index.php
+++ upgrade_check_tool/index.php
@@ -1,5 +1,6 @@
 <?php
 
+log_write('info', 'Checking for presence of files.md5 file.');
 if (file_exists("files.md5")) {
     include 'files.md5';
     $bad_files = array();
@@ -11,7 +12,14 @@ if (file_exists("files.md5")) {
             }
         }
     }
-    var_dump($bad_files);
+    foreach ($bad_files as $file) {
+        log_write('warn', 'File ' . realpath($file) . ' is not how it was shipped.');
+    }
 } else {
-    // Throw error due to not being able to check validity of base files.
+    log_write('error', 'Could not check for file validity: files.md5 is missing.');
+}
+
+function log_write($level, $message)
+{
+    echo strtoupper($level) . ": " . $message . "</br>";
 }
